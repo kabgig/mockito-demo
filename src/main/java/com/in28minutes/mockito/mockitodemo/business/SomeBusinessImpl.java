09bbf8ba09071a83d17e8032e2d0b74898864396
Ansar Ismagilov <ansar89kab@bk.ru>
2023-08-07 17:19:11 +0800

    some initial logic

---
+++ src/main/java/com/in28minutes/mockito/mockitodemo/business/SomeBusinessImpl.java
@@ -0,0 +1,18 @@
+package com.in28minutes.mockito.mockitodemo.business;
+
+public class SomeBusinessImpl {
+    private DateService dateService;
+    public int findTheGreatestFromTheData(){
+        int[] data = dateService.retrieveAllData();
+        int greatestValue = Integer.MIN_VALUE;
+        for(int value: data){
+            if (value > greatestValue) greatestValue = value;
+        }
+        return greatestValue;
+
+    }
+}
+
+interface DateService {
+    int[] retrieveAllData();
+}
