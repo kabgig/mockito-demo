342966779c8dda2c8f000f3698623fcff6f268ae
Ansar Ismagilov <ansar89kab@bk.ru>
2023-08-07 19:02:37 +0800

    mockito went through

--- src/test/java/com/in28minutes/mockito/mockitodemo/list/ListTest.java
+++ src/test/java/com/in28minutes/mockito/mockitodemo/list/ListTest.java
@@ -11,29 +11,30 @@ import static org.mockito.Mockito.when;
 
 class ListTest {
 
+    List listMock = mock(List.class);
+
     @Test
     void simpleTest() {
-        List listMock = mock(List.class);
         when(listMock.size()).thenReturn(3);
         assertEquals(3, listMock.size());
     }
+
     @Test
     void multipleReturns() {
-        List listMock = mock(List.class);
         when(listMock.size()).thenReturn(1).thenReturn(2);
         assertEquals(1, listMock.size());
         assertEquals(2, listMock.size());
     }
+
     @Test
     void specificParameters() {
-        List listMock = mock(List.class);
         when(listMock.get(0)).thenReturn("SomeString");
         assertEquals("SomeString", listMock.get(0));
         assertEquals(null, listMock.get(1));
     }
+
     @Test
     void genericParameters() {
-        List listMock = mock(List.class);
         when(listMock.get(Mockito.anyInt())).thenReturn("SomeOtherString");
         assertEquals("SomeOtherString", listMock.get(0));
         assertEquals("SomeOtherString", listMock.get(1));
